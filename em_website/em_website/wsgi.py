5aee752783b505f5d6a7bb983d4882d493197e99
Thejesh PR <tp-r@microfocus.com>
2021-03-25 19:42:26 +0530

    updated wsgi

--- em_website/em_website/wsgi.py
+++ em_website/em_website/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'em_website.settings')
 
-application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
+# application = get_wsgi_application()
