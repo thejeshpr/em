757500c10b43d1fe012d193960257d8a41273f64
Thejesh PR <tp-r@microfocus.com>
2021-03-25 19:19:20 +0530

    removed dj_database_url

--- em_website/em_website/settings.py
+++ em_website/em_website/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
