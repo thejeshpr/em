23417330b1ecf494279885a40a21f748ac97afcf
Thejesh PR <tp-r@microfocus.com>
2021-03-25 19:57:18 +0530

    test2

--- em_website/em_website/wsgi.py
+++ em_website/em_website/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'em_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'em_website.em_website.settings')
 
 application = get_wsgi_application()
 
