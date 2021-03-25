86e13717d1dcc971bb24c6f7fae05ef5fe591898
Thejesh PR <tp-r@microfocus.com>
2021-03-25 19:59:45 +0530

    added db

--- em_website/em_website/settings.py
+++ em_website/em_website/settings.py
@@ -16,8 +16,7 @@ import django_heroku
 
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,6 +91,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
